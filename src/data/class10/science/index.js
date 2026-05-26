7268b30c1eda7397e010787e5af21d89c1660c8c
Hrithik Gupta <hrithik13gupta@gmail.com>
2026-05-26 09:43:36 +0530

    Add Class 10 Science PYQ questions - all 13 chapters

---
+++ src/data/class10/science/index.js
@@ -0,0 +1,46 @@
+// CLASS 10 SCIENCE — ALL CHAPTERS PYQ INDEX
+import { chemicalReactionsAndEquationsQuestions } from "./chemicalReactionsAndEquations"
+import { acidsBasesAndSaltsQuestions } from "./acidsBasesAndSalts"
+import { metalsAndNonMetalsQuestions } from "./metalsAndNonMetals"
+import { carbonAndItsCompoundsQuestions } from "./carbonAndItsCompounds"
+import { lifeProcessesQuestions } from "./lifeProcesses"
+import { controlAndCoordinationQuestions } from "./controlAndCoordination"
+import { howDoOrganismsReproduceQuestions } from "./howDoOrganismsReproduce"
+import { heredityAndEvolutionQuestions } from "./heredityAndEvolution"
+import { lightReflectionAndRefractionQuestions } from "./lightReflectionAndRefraction"
+import { humanEyeAndColourfulWorldQuestions } from "./humanEyeAndColourfulWorld"
+import { electricityQuestions } from "./electricity"
+import { magneticEffectsOfElectricCurrentQuestions } from "./magneticEffectsOfElectricCurrent"
+import { sourcesOfEnergyQuestions } from "./sourcesOfEnergy"
+
+export const class10ScienceQuestions = [
+  ...chemicalReactionsAndEquationsQuestions,
+  ...acidsBasesAndSaltsQuestions,
+  ...metalsAndNonMetalsQuestions,
+  ...carbonAndItsCompoundsQuestions,
+  ...lifeProcessesQuestions,
+  ...controlAndCoordinationQuestions,
+  ...howDoOrganismsReproduceQuestions,
+  ...heredityAndEvolutionQuestions,
+  ...lightReflectionAndRefractionQuestions,
+  ...humanEyeAndColourfulWorldQuestions,
+  ...electricityQuestions,
+  ...magneticEffectsOfElectricCurrentQuestions,
+  ...sourcesOfEnergyQuestions,
+]
+
+export {
+  chemicalReactionsAndEquationsQuestions,
+  acidsBasesAndSaltsQuestions,
+  metalsAndNonMetalsQuestions,
+  carbonAndItsCompoundsQuestions,
+  lifeProcessesQuestions,
+  controlAndCoordinationQuestions,
+  howDoOrganismsReproduceQuestions,
+  heredityAndEvolutionQuestions,
+  lightReflectionAndRefractionQuestions,
+  humanEyeAndColourfulWorldQuestions,
+  electricityQuestions,
+  magneticEffectsOfElectricCurrentQuestions,
+  sourcesOfEnergyQuestions,
+}
