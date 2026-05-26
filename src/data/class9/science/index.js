4edd81adc1d351dd4bbdceb1facc66b7b6b4adb1
Hrithik Gupta <hrithik13gupta@gmail.com>
2026-05-26 11:08:27 +0530

    Add Class 9 Science PYQ questions - all 12 chapters

---
+++ src/data/class9/science/index.js
@@ -0,0 +1,43 @@
+// CLASS 9 SCIENCE — ALL CHAPTERS PYQ INDEX
+import { matterInOurSurroundingsQuestions } from "./matterInOurSurroundings"
+import { isMatterAroundUsPureQuestions } from "./isMatterAroundUsPure"
+import { atomsAndMoleculesQuestions } from "./atomsAndMolecules"
+import { structureOfTheAtomQuestions } from "./structureOfTheAtom"
+import { theFundamentalUnitOfLifeQuestions } from "./theFundamentalUnitOfLife"
+import { tissuesQuestions } from "./tissues"
+import { motionQuestions } from "./motion"
+import { forceAndLawsOfMotionQuestions } from "./forceAndLawsOfMotion"
+import { gravitationQuestions } from "./gravitation"
+import { workAndEnergyQuestions } from "./workAndEnergy"
+import { soundQuestions } from "./sound"
+import { improvementInFoodResourcesQuestions } from "./improvementInFoodResources"
+
+export const class9ScienceQuestions = [
+  ...matterInOurSurroundingsQuestions,
+  ...isMatterAroundUsPureQuestions,
+  ...atomsAndMoleculesQuestions,
+  ...structureOfTheAtomQuestions,
+  ...theFundamentalUnitOfLifeQuestions,
+  ...tissuesQuestions,
+  ...motionQuestions,
+  ...forceAndLawsOfMotionQuestions,
+  ...gravitationQuestions,
+  ...workAndEnergyQuestions,
+  ...soundQuestions,
+  ...improvementInFoodResourcesQuestions,
+]
+
+export {
+  matterInOurSurroundingsQuestions,
+  isMatterAroundUsPureQuestions,
+  atomsAndMoleculesQuestions,
+  structureOfTheAtomQuestions,
+  theFundamentalUnitOfLifeQuestions,
+  tissuesQuestions,
+  motionQuestions,
+  forceAndLawsOfMotionQuestions,
+  gravitationQuestions,
+  workAndEnergyQuestions,
+  soundQuestions,
+  improvementInFoodResourcesQuestions,
+}
